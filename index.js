e5c498002bbb8fe706784963535178bd07810262
Pierre Lavalette <lavalette.pierre@gmail.com>
2020-05-28 13:24:26 +0200

    Server Step 1.3: Setup a Express with a basic health check route

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('hello world');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.get('/_ping', (req, res) => {
+    res.send('pong');
+});
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
