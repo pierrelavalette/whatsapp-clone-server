7275c8568b9f520346c7b541222308c33b8487e3
Pierre Lavalette <lavalette.pierre@gmail.com>
2020-05-28 14:54:48 +0200

    Server Step 1.5: Create GET /chats route

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
-import express from "express";
+import express from 'express';
+import { chats } from './db';
 
 const app = express();
 
-app.get("/_ping", (req, res) => {
-  res.send("pong");
+app.get('/_ping', (req, res) => {
+  res.send('pong');
 });
 
+app.get('/chats', (req, res) => {
+  res.json(chats);
+});
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
