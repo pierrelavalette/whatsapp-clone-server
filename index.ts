428efd5a9b94fea330923917a2f9c366d8b33fe0
Pierre Lavalette <lavalette.pierre@gmail.com>
2020-05-29 12:24:39 +0200

    Server Step 2.3: Resolve Chat.lastMessage

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
+import { ApolloServer, gql } from 'apollo-server-express';
 import express from 'express';
 import { chats } from './db';
 import cors from 'cors';
+import schema from './schema';
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/_ping', (req, res) => {
   res.send('pong');
@@ -13,6 +16,14 @@ app.get('/_ping', (req, res) => {
 app.get('/chats', (req, res) => {
   res.json(chats);
 });
+
+const server = new ApolloServer({ schema });
+
+server.applyMiddleware({
+  app,
+  path: '/graphql',
+});
+
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
