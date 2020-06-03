3849a2ffa81d9bd82d506b4489b585b3d6a80f22
Pierre Lavalette <lavalette.pierre@gmail.com>
2020-06-03 16:05:28 +0200

    Server Step 4.3: Add chat field to Query type

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -6,17 +6,21 @@ const resolvers = {
   URL: URLResolver,
 
   Chat: {
-    lastMessage(chat: any) {
-      return messages.find((m) => m.id === chat.lastMessage);
-    },
     messages(chat: any) {
       return messages.filter((m) => chat.messages.includes(m.id));
+    },
+    lastMessage(chat: any) {
+      const lastMessage = chat.messages[chat.messages.length - 1];
+      return messages.find((m) => m.id === lastMessage);
     },
   },
 
   Query: {
     chats() {
       return chats;
+    },
+    chat(root: any, { chatId }: any) {
+      return chats.find((c) => c.id === chatId);
     },
   },
 };
